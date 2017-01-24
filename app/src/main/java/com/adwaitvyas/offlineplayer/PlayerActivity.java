544a01c40abaff6835e994429acded457de5f2ef
adwaitvyas <adwait.vyas@pagalguy.com>
2017-01-24 16:59:39 +0530

    Online player works now! woohoo!

--- app/src/main/java/com/adwaitvyas/offlineplayer/PlayerActivity.java
+++ app/src/main/java/com/adwaitvyas/offlineplayer/PlayerActivity.java
@@ -2,15 +2,86 @@ package com.adwaitvyas.offlineplayer;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.extractor.ExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveVideoTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class PlayerActivity extends AppCompatActivity {
 
+    private SimpleExoPlayer player;
+    @BindView(R.id.videoView) SimpleExoPlayerView simpleExoPlayerView;
+    private String videoUrl;
+    private static final DefaultBandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+
+    // Activity onCreate
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
+        ButterKnife.bind(this);
+        videoUrl = getIntent().getStringExtra("videoUrl");
+        createPlayer();
+        simpleExoPlayerView.setPlayer(player);
+        preparePlayer();
+    }
+
+    private void createPlayer() {
+        // Create a default TrackSelector
+        Handler mainHandler = new Handler();
+        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        TrackSelection.Factory videoTrackSelectionFactory =
+                new AdaptiveVideoTrackSelection.Factory(bandwidthMeter);
+        TrackSelector trackSelector =
+                new DefaultTrackSelector(videoTrackSelectionFactory);
+        // Create a default LoadControl
+        LoadControl loadControl = new DefaultLoadControl();
+        // Create the player
+        player = ExoPlayerFactory.newSimpleInstance(this, trackSelector, loadControl);
+    }
+
+    private void preparePlayer() {
+        // Measures bandwidth during playback. Can be null if not required.
+        DefaultBandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        // Produces DataSource instances through which media data is loaded.
+        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(this,
+                Util.getUserAgent(this, "OfflinePlayer"), bandwidthMeter);
+        // Produces Extractor instances for parsing the media data.
+        ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+        // This is the MediaSource representing the media to be played.
+        MediaSource videoSource = new ExtractorMediaSource(Uri.parse(videoUrl),
+                dataSourceFactory, extractorsFactory, null, null);
+        // Prepare the player with the source and play when ready
+        player.setPlayWhenReady(true);
+        player.prepare(videoSource);
+    }
+
+    @Override
+    protected void onStop() {
+        player.release();
+        super.onStop();
     }
 
     public static Intent getStartingIntent(Context context, String videoUrl) {
@@ -18,4 +89,6 @@ public class PlayerActivity extends AppCompatActivity {
         intent.putExtra("videoUrl", videoUrl);
         return intent;
     }
+
+
 }
